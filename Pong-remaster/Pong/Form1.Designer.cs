ea2fba04f4bf8e3e6e561142afc01dfa5a3436f8
ScoutJLY <ScoutJLY@users.noreply.github.com>
2018-04-30 11:53:32 +0800

    first commit

---
+++ Pong-remaster/Pong/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿using System.Windows.Forms;
+
+namespace Pong
+{
+    partial class PongForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(307, 224);
+            this.scoreLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(657, 140);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "Player 1 v Player 2\r\n       0 - 0";
+            this.scoreLabel.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PongForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 677);
+            this.Controls.Add(this.scoreLabel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PongForm";
+            this.Text = "PongGame";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PongForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PongForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.PongForm_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label scoreLabel;
+        private Timer timer1;
+    }
+}
+
